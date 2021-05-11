2adaefa05b7a5cff1a8e97eb68919ec3a74b37f9
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2021-05-11 21:11:34 +0530

    Push api_gwv2 module usage

--- api_gateway.tf
+++ api_gateway.tf
@@ -30,7 +30,6 @@ resource "aws_api_gateway_account" "api_gateway" {
 /*
   rest is API Gateway specific to External Functions
 */
-
 resource "aws_iam_role" "gateway_caller" {
   name = "${var.prefix}-api-gateway-caller"
   path = "/"
@@ -73,15 +72,6 @@ resource "aws_iam_role_policy_attachment" "gateway_caller" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayInvokeFullAccess"
 }
 
-resource "aws_api_gateway_rest_api" "ef_to_lambda" {
-  name = "${var.prefix}-seceng-external-functions"
-  endpoint_configuration {
-    types = [
-      "REGIONAL",
-    ]
-  }
-}
-
 resource "aws_api_gateway_rest_api_policy" "ef_to_lambda" {
   depends_on = [
     aws_api_gateway_rest_api.ef_to_lambda,
@@ -106,213 +96,214 @@ resource "aws_api_gateway_rest_api_policy" "ef_to_lambda" {
   })
 }
 
-resource "aws_api_gateway_resource" "https" {
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
-  path_part   = "https"
-}
+# resource "aws_api_gateway_resource" "https" {
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
+#   path_part   = "https"
+# }
 
-resource "aws_api_gateway_method" "https_post" {
-  rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id    = aws_api_gateway_resource.https.id
-  http_method    = "POST"
-  authorization  = "AWS_IAM"
-  request_models = {}
-  request_parameters = {
-    "method.request.header.sf-custom-base-url"      = false
-    "method.request.header.sf-custom-url"           = false
-    "method.request.header.sf-custom-method"        = false
-    "method.request.header.sf-custom-headers"       = false
-    "method.request.header.sf-custom-params"        = false
-    "method.request.header.sf-custom-data"          = false
-    "method.request.header.sf-custom-json"          = false
-    "method.request.header.sf-custom-timeout"       = false
-    "method.request.header.sf-custom-auth"          = false
-    "method.request.header.sf-custom-response-type" = false
-    "method.request.header.sf-custom-verbose"       = false
-  }
-}
+# resource "aws_api_gateway_method" "https_post" {
+#   rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id    = aws_api_gateway_resource.https.id
+#   http_method    = "POST"
+#   authorization  = "AWS_IAM"
+#   request_models = {}
+#   request_parameters = {
+#     "method.request.header.sf-custom-base-url"      = false
+#     "method.request.header.sf-custom-url"           = false
+#     "method.request.header.sf-custom-method"        = false
+#     "method.request.header.sf-custom-headers"       = false
+#     "method.request.header.sf-custom-params"        = false
+#     "method.request.header.sf-custom-data"          = false
+#     "method.request.header.sf-custom-json"          = false
+#     "method.request.header.sf-custom-timeout"       = false
+#     "method.request.header.sf-custom-auth"          = false
+#     "method.request.header.sf-custom-response-type" = false
+#     "method.request.header.sf-custom-verbose"       = false
+#   }
+# }
 
-resource "aws_api_gateway_integration" "https_to_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id             = aws_api_gateway_resource.https.id
-  http_method             = aws_api_gateway_method.https_post.http_method
-  integration_http_method = aws_api_gateway_method.https_post.http_method
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  timeout_milliseconds    = 29000
-  uri                     = aws_lambda_function.geff_lambda.invoke_arn
-  cache_key_parameters    = null
-  request_parameters      = {}
-  request_templates       = {}
-}
+# resource "aws_api_gateway_integration" "https_to_lambda" {
+#   rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id             = aws_api_gateway_resource.https.id
+#   http_method             = aws_api_gateway_method.https_post.http_method
+#   integration_http_method = aws_api_gateway_method.https_post.http_method
+#   type                    = "AWS_PROXY"
+#   content_handling        = "CONVERT_TO_TEXT"
+#   timeout_milliseconds    = 29000
+#   uri                     = aws_lambda_function.geff_lambda.invoke_arn
+#   cache_key_parameters    = null
+#   request_parameters      = {}
+#   request_templates       = {}
+# }
 
-resource "aws_api_gateway_resource" "smtp" {
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
-  path_part   = "smtp"
-}
+# resource "aws_api_gateway_resource" "smtp" {
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
+#   path_part   = "smtp"
+# }
 
-resource "aws_api_gateway_method" "smtp_post" {
-  rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id    = aws_api_gateway_resource.smtp.id
-  http_method    = "POST"
-  authorization  = "AWS_IAM"
-  request_models = {}
-  request_parameters = {
-    "method.request.header.sf-custom-host"      = false
-    "method.request.header.sf-custom-port"      = false
-    "method.request.header.sf-custom-user"      = false
-    "method.request.header.sf-custom-password"  = false
-    "method.request.header.sf-custom-recipient" = false
-    "method.request.header.sf-custom-subject"   = false
-    "method.request.header.sf-custom-text"      = false
-  }
-}
+# resource "aws_api_gateway_method" "smtp_post" {
+#   rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id    = aws_api_gateway_resource.smtp.id
+#   http_method    = "POST"
+#   authorization  = "AWS_IAM"
+#   request_models = {}
+#   request_parameters = {
+#     "method.request.header.sf-custom-host"      = false
+#     "method.request.header.sf-custom-port"      = false
+#     "method.request.header.sf-custom-user"      = false
+#     "method.request.header.sf-custom-password"  = false
+#     "method.request.header.sf-custom-recipient" = false
+#     "method.request.header.sf-custom-subject"   = false
+#     "method.request.header.sf-custom-text"      = false
+#   }
+# }
 
-resource "aws_api_gateway_integration" "smtp_to_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id             = aws_api_gateway_resource.smtp.id
-  http_method             = aws_api_gateway_method.smtp_post.http_method
-  integration_http_method = aws_api_gateway_method.smtp_post.http_method
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  timeout_milliseconds    = 29000
-  uri                     = aws_lambda_function.geff_lambda.invoke_arn
-  cache_key_parameters    = null
-  request_parameters      = {}
-  request_templates       = {}
-}
+# resource "aws_api_gateway_integration" "smtp_to_lambda" {
+#   rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id             = aws_api_gateway_resource.smtp.id
+#   http_method             = aws_api_gateway_method.smtp_post.http_method
+#   integration_http_method = aws_api_gateway_method.smtp_post.http_method
+#   type                    = "AWS_PROXY"
+#   content_handling        = "CONVERT_TO_TEXT"
+#   timeout_milliseconds    = 29000
+#   uri                     = aws_lambda_function.geff_lambda.invoke_arn
+#   cache_key_parameters    = null
+#   request_parameters      = {}
+#   request_templates       = {}
+# }
 
-resource "aws_api_gateway_resource" "cloudwatch_metric" {
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
-  path_part   = "cloudwatch_metric"
-}
+# resource "aws_api_gateway_resource" "cloudwatch_metric" {
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
+#   path_part   = "cloudwatch_metric"
+# }
 
-resource "aws_api_gateway_method" "cloudwatch_metric_post" {
-  rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id    = aws_api_gateway_resource.cloudwatch_metric.id
-  http_method    = "POST"
-  authorization  = "AWS_IAM"
-  request_models = {}
-  request_parameters = {
-    "method.request.header.sf-custom-namespace"  = false
-    "method.request.header.sf-custom-name"       = false
-    "method.request.header.sf-custom-dimensions" = false
-    "method.request.header.sf-custom-unit"       = false
-    "method.request.header.sf-custom-value"      = false
-    "method.request.header.sf-custom-region"     = false
-    "method.request.header.sf-custom-timestamp"  = false
-  }
-}
+# resource "aws_api_gateway_method" "cloudwatch_metric_post" {
+#   rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id    = aws_api_gateway_resource.cloudwatch_metric.id
+#   http_method    = "POST"
+#   authorization  = "AWS_IAM"
+#   request_models = {}
+#   request_parameters = {
+#     "method.request.header.sf-custom-namespace"  = false
+#     "method.request.header.sf-custom-name"       = false
+#     "method.request.header.sf-custom-dimensions" = false
+#     "method.request.header.sf-custom-unit"       = false
+#     "method.request.header.sf-custom-value"      = false
+#     "method.request.header.sf-custom-region"     = false
+#     "method.request.header.sf-custom-timestamp"  = false
+#   }
+# }
 
-resource "aws_api_gateway_integration" "cloudwatch_metric_to_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id             = aws_api_gateway_resource.cloudwatch_metric.id
-  http_method             = aws_api_gateway_method.cloudwatch_metric_post.http_method
-  integration_http_method = aws_api_gateway_method.cloudwatch_metric_post.http_method
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  timeout_milliseconds    = 29000
-  uri                     = aws_lambda_function.geff_lambda.invoke_arn
-  cache_key_parameters    = null
-  request_parameters      = {}
-  request_templates       = {}
-}
+# resource "aws_api_gateway_integration" "cloudwatch_metric_to_lambda" {
+#   rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id             = aws_api_gateway_resource.cloudwatch_metric.id
+#   http_method             = aws_api_gateway_method.cloudwatch_metric_post.http_method
+#   integration_http_method = aws_api_gateway_method.cloudwatch_metric_post.http_method
+#   type                    = "AWS_PROXY"
+#   content_handling        = "CONVERT_TO_TEXT"
+#   timeout_milliseconds    = 29000
+#   uri                     = aws_lambda_function.geff_lambda.invoke_arn
+#   cache_key_parameters    = null
+#   request_parameters      = {}
+#   request_templates       = {}
+# }
 
-resource "aws_api_gateway_resource" "boto3" {
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
-  path_part   = "boto3"
-}
+# resource "aws_api_gateway_resource" "boto3" {
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
+#   path_part   = "boto3"
+# }
 
-resource "aws_api_gateway_method" "boto3_post" {
-  rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id    = aws_api_gateway_resource.boto3.id
-  http_method    = "POST"
-  authorization  = "AWS_IAM"
-  request_models = {}
-  request_parameters = {
-    "method.request.header.sf-custom-Namespace"     = false
-    "method.request.header.sf-custom-MetricData"    = false
-    "method.request.header.sf-custom-logGroupName"  = false
-    "method.request.header.sf-custom-logStreamName" = false
-  }
-}
+# resource "aws_api_gateway_method" "boto3_post" {
+#   rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id    = aws_api_gateway_resource.boto3.id
+#   http_method    = "POST"
+#   authorization  = "AWS_IAM"
+#   request_models = {}
+#   request_parameters = {
+#     "method.request.header.sf-custom-Namespace"     = false
+#     "method.request.header.sf-custom-MetricData"    = false
+#     "method.request.header.sf-custom-logGroupName"  = false
+#     "method.request.header.sf-custom-logStreamName" = false
+#   }
+# }
 
-resource "aws_api_gateway_integration" "boto3_to_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id             = aws_api_gateway_resource.boto3.id
-  http_method             = aws_api_gateway_method.boto3_post.http_method
-  integration_http_method = aws_api_gateway_method.boto3_post.http_method
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  timeout_milliseconds    = 29000
-  uri                     = aws_lambda_function.geff_lambda.invoke_arn
-  cache_key_parameters    = null
-  request_parameters      = {}
-  request_templates       = {}
-}
+# resource "aws_api_gateway_integration" "boto3_to_lambda" {
+#   rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id             = aws_api_gateway_resource.boto3.id
+#   http_method             = aws_api_gateway_method.boto3_post.http_method
+#   integration_http_method = aws_api_gateway_method.boto3_post.http_method
+#   type                    = "AWS_PROXY"
+#   content_handling        = "CONVERT_TO_TEXT"
+#   timeout_milliseconds    = 29000
+#   uri                     = aws_lambda_function.geff_lambda.invoke_arn
+#   cache_key_parameters    = null
+#   request_parameters      = {}
+#   request_templates       = {}
+# }
 
-resource "aws_api_gateway_resource" "xmlrpc" {
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
-  path_part   = "xml-rpc"
-}
+# resource "aws_api_gateway_resource" "xmlrpc" {
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   parent_id   = aws_api_gateway_rest_api.ef_to_lambda.root_resource_id
+#   path_part   = "xml-rpc"
+# }
 
-resource "aws_api_gateway_method" "xmlrpc_post" {
-  rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id    = aws_api_gateway_resource.xmlrpc.id
-  http_method    = "POST"
-  authorization  = "AWS_IAM"
-  request_models = {}
-  request_parameters = {
-    "method.request.header.sf-custom-url" = false
-  }
-}
+# resource "aws_api_gateway_method" "xmlrpc_post" {
+#   rest_api_id    = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id    = aws_api_gateway_resource.xmlrpc.id
+#   http_method    = "POST"
+#   authorization  = "AWS_IAM"
+#   request_models = {}
+#   request_parameters = {
+#     "method.request.header.sf-custom-url" = false
+#   }
+# }
 
-resource "aws_api_gateway_integration" "xmlrpc_to_lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
-  resource_id             = aws_api_gateway_resource.xmlrpc.id
-  http_method             = aws_api_gateway_method.xmlrpc_post.http_method
-  integration_http_method = aws_api_gateway_method.xmlrpc_post.http_method
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  timeout_milliseconds    = 29000
-  uri                     = aws_lambda_function.geff_lambda.invoke_arn
-  cache_key_parameters    = null
-  request_parameters      = {}
-  request_templates       = {}
-}
+# resource "aws_api_gateway_integration" "xmlrpc_to_lambda" {
+#   rest_api_id             = aws_api_gateway_rest_api.ef_to_lambda.id
+#   resource_id             = aws_api_gateway_resource.xmlrpc.id
+#   http_method             = aws_api_gateway_method.xmlrpc_post.http_method
+#   integration_http_method = aws_api_gateway_method.xmlrpc_post.http_method
+#   type                    = "AWS_PROXY"
+#   content_handling        = "CONVERT_TO_TEXT"
+#   timeout_milliseconds    = 29000
+#   uri                     = aws_lambda_function.geff_lambda.invoke_arn
+#   cache_key_parameters    = null
+#   request_parameters      = {}
+#   request_templates       = {}
+# }
+
+
+# resource "aws_api_gateway_stage" "geff" {
+#   deployment_id = aws_api_gateway_deployment.geff.id
+#   rest_api_id   = aws_api_gateway_rest_api.ef_to_lambda.id
+#   stage_name    = var.env
+# }
+
+# resource "aws_api_gateway_deployment" "geff" {
+#   depends_on = [
+#     aws_api_gateway_integration.https_to_lambda,
+#     aws_api_gateway_integration.smtp_to_lambda,
+#     aws_api_gateway_integration.boto3_to_lambda,
+#     aws_api_gateway_integration.xmlrpc_to_lambda,
+#     aws_api_gateway_integration.cloudwatch_metric_to_lambda,
+#   ]
+#   triggers = {
+#     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.ef_to_lambda))
+#   }
+
+#   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
 resource "aws_cloudwatch_log_group" "api_gateway" {
   name              = "api_gw_${aws_api_gateway_rest_api.ef_to_lambda.id}/${var.env}"
   retention_in_days = var.log_retention_days
-}
-
-resource "aws_api_gateway_deployment" "geff" {
-  depends_on = [
-    aws_api_gateway_integration.https_to_lambda,
-    aws_api_gateway_integration.smtp_to_lambda,
-    aws_api_gateway_integration.boto3_to_lambda,
-    aws_api_gateway_integration.xmlrpc_to_lambda,
-    aws_api_gateway_integration.cloudwatch_metric_to_lambda,
-  ]
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.ef_to_lambda))
-  }
-
-  rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "geff" {
-  deployment_id = aws_api_gateway_deployment.geff.id
-  rest_api_id   = aws_api_gateway_rest_api.ef_to_lambda.id
-  stage_name    = var.env
 }
 
 resource "aws_api_gateway_method_settings" "enable_logging" {
@@ -320,6 +311,7 @@ resource "aws_api_gateway_method_settings" "enable_logging" {
   rest_api_id = aws_api_gateway_rest_api.ef_to_lambda.id
   stage_name  = aws_api_gateway_stage.geff.stage_name
   method_path = "*/*"
+
   settings {
     logging_level          = "INFO"
     metrics_enabled        = true
@@ -327,3 +319,36 @@ resource "aws_api_gateway_method_settings" "enable_logging" {
     throttling_rate_limit  = 10000
   }
 }
+
+
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
+  name          = "api-gateway-geff"
+  description   = "HTTPS API Gateway for slack to geff traffic."
+  protocol_type = "HTTPS"
+  create_api_domain_name                   = false
+  default_stage_access_log_destination_arn = aws_cloudwatch_log_group.api_gateway.arn
+  #default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+  integrations = {
+    "ANY /" = {
+      lambda_arn             = aws_lambda_function.dispatch_slack.arn
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+    "$default" = {
+      lambda_arn = aws_lambda_function.dispatch_slack.arn
+    }
+  }
+  tags = {
+    Name = "api-gateway-geff"
+  }
+}
+
+# resource "aws_api_gateway_rest_api" "ef_to_lambda" {
+#   name = "${var.prefix}-seceng-external-functions"
+#   endpoint_configuration {
+#     types = [
+#       "REGIONAL",
+#     ]
+#   }
+# }
